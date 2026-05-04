4b836bb58260ac5d939544c8fc2a9e7c6e41e7df
Willams <williamekene700@gmail.com>
2026-05-04 13:31:34 +0100

    add findbytradeid for trade record repository

--- src/main/java/com/emporia/backend/repository/TradeRecordRepository.java
+++ src/main/java/com/emporia/backend/repository/TradeRecordRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TradeRecordRepository extends JpaRepository<TradeRecord, Long> {
     List<TradeRecord> findBySeller(SMEProfile seller);
     List<TradeRecord> findByBuyer(SMEProfile buyer);
+    Optional<TradeRecord> findByTradeId(String tradeId);
 }
